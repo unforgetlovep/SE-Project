dfc2fc199c88d33d7a0731e92e1d3beb83d3d0f8
Choice <1275537624@qq.com>
2024-11-15 21:28:23 +0800

    123

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 using namespace std;
 int main() {
+<<<<<<< HEAD
 	cout << "hello shenzhen" << endl;
+=======
+	cout << "hello WORLD" << endl;
+>>>>>>> 2bc8725 (fetch-first)
 	cout << "hello SZTU"<<endl;
-
+	cout << "hello BDI" << endl;
 
 }
