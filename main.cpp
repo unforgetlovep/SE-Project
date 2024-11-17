7928f8d55d69a92b4c5190f54fdd05547abf672c
PCM <1970125842@qq.com>
2024-11-17 17:07:33 +0800

    pcm-commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include<iostream>
 using namespace std;
 int main() {
-<<<<<<< HEAD
-	cout << "hello shenzhen" << endl;
-=======
-	cout << "hello WORLD" << endl;
->>>>>>> 2bc8725 (fetch-first)
+	cout << "hello Chinese" << endl;//pcm commit
 	cout << "hello SZTU"<<endl;
 	cout << "hello BDI" << endl;
 // zhy commit
